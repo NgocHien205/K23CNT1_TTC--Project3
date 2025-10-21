b1ba3257da009d2de9cd6dc69d16d8cd032de872
NgocHien205 <nguyenhien13062005@gmail.com>
2025-10-21 17:05:18 +0700

    Day02

---
+++ Learing Spring Boot Core  - day02  (1)/Learing-Spring-Boot-Core-day02/src/main/java/com/example/demo/LearingSpringBootCoreDay02Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.*;
+
+@SpringBootApplication
+public class LearingSpringBootCoreDay02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearingSpringBootCoreDay02Application.class, args);
+        System.out.println("Nguyen Ngoc Hien - 2310900033");
+	}
+
+}
